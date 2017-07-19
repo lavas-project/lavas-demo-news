4748454a2e31f63edd9e4be68ec9ed3a0a29146d
zhangzhiqiang37 <zhangzhiqiang04@baidu.com>
2017-07-19 15:18:24 +0800

    skeleton调整

--- src/App.vue
+++ src/App.vue
@@ -85,6 +85,7 @@ export default {
     -moz-osx-font-smoothing grayscale
     // text-align center
     color #2c3e50
+    height: 100%
 
 </style>
 
--- src/pages/Skeleton.vue
+++ src/pages/Skeleton.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="skeleton-wrapper">
         <section class="skeleton-block">
+            <img src="../../static/img/skeleton-logo.jpg" class="skeleton-logo">
             <div class="title" v-show="showTitle" ref="title">
                 <img src="../../static/img/skeleton-bg.jpg" class="skeleton-bg">
             </div>
         </section>
-        <img src="../../static/img/skeleton-logo.jpg" class="skeleton-logo">
     </div>
 </template>
 
@@ -28,27 +28,31 @@ export default {
 
 .skeleton-wrapper
     background #fff
+    height: 100%
 
     .skeleton-block
         width 100%
         height 100%
         color #fff
         text-align center
+        display: flex
+        flex-direction: column-reverse
 
         .title
-            position absolute
-            text-align: center
             width: 100%
-            top 50%
-            transform translateY(-50%)
+            flex: 1
+            position: relative
             .skeleton-bg
                 width: (530 / 750) * 100%
+                position: absolute
+                top: 50%
+                left: 50%
+                transform: translate(-50%, -50%)
     .skeleton-logo
         width: (350 / 750) * 100%
-        position: absolute
-        bottom: 30px
+        margin-bottom: 25px
+        position: relative
         left: 50%
-        transform translateX(-50%)
-
+        transform: translateX(-50%)
 
 </style>
