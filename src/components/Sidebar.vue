4a4114f36f7ee46f098a13fe227763a5f7d2dd73
qiusiqi <qiusiqi@baidu.com>
2017-07-20 14:45:26 +0800

    reset

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -276,8 +276,8 @@ export default {
          */
         changeOpacity() {
             if (this.wrapperClass.expand && this.iscroll) {
-                //this.opacity = (this.iscroll.x + this.itsWidth) / this.itsWidth * 0.5;
-                setTimeout(() => {this.opacity = (this.iscroll.x + this.itsWidth) / this.itsWidth * 0.5;}, 80);
+                this.opacity = (this.iscroll.x + this.itsWidth) / this.itsWidth * 0.5;
+                //setTimeout(() => {this.opacity = (this.iscroll.x + this.itsWidth) / this.itsWidth * 0.5;}, 80);
                 rAF(this.changeOpacity.bind(this));
             }
         }
